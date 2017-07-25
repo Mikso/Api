87575b3a3d3c4eea0c4ecb09bedb7610f2200969
Mikso <bmarcos330@gmail.com>
2017-07-25 15:07:08 -0300

    Segunda alteração

--- Celular/src/Main.java
+++ Celular/src/Main.java
@@ -8,6 +8,8 @@
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Ola");
+		new Publica().exibe("Bruno");
+		
+		
 	}
 }
---
+++ Celular/src/Publica.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+/**
+ * @author Bruno
+ *
+ */
+public class Publica {
+	public void exibe(String nomeElemento){
+		System.out.println("O nome impresso foi " + nomeElemento);
+	}
+}
