72f71477430b02dfcdb9ea0e3f30a840d6315ea9
Mikso <bmarcos330@gmail.com>
2017-07-25 15:14:34 -0300

    Outra

--- Celular/src/Publica.java
+++ Celular/src/Publica.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -9,5 +11,9 @@
 public class Publica {
 	public void exibe(String nomeElemento){
 		System.out.println("O nome impresso foi " + nomeElemento);
+		ArrayList<String> nt = new ArrayList<>();
+		nt.add(nomeElemento);
+		System.out.println(nt.contains(nomeElemento));
+		
 	}
 }
